35921888854123071ae9c32bf6a76e141060c0a1
Julian Souder <souderj@xavier.edu>
2020-11-08 15:45:51 -0500

    Correct the c component in sphere hit method

--- sphere.h
+++ sphere.h
@@ -16,7 +16,7 @@ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const 
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
     float b = dot(oc, r.direction());
-    float c = dot(oc, oc);
+    float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - a*c;
     if (discriminant > 0) {
         float temp = (-b - sqrt(b*b - a*c)) / a;
